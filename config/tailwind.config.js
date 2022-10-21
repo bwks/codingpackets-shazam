f111dc9d9338e938b6d55b769c852ec80afe9f2f
Brad Searle <bradleysearle@gmail.com>
2022-10-21 15:58:23 +1000

    style

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'codingpackets/_site/**/*.{html,js}'
+    // 'codingpackets/_site/*.{html,js}',
+    'codingpackets/_site/blog/**/*.html'
   ],
   safelist: [],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Ubuntu"],
+        sans: ["Bree Serif"],
         mono: ["Ubuntu Mono"],
       },
     },
