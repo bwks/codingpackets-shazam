95f125404efe71b1b8ab14e3eb210e349965f700
Brad Searle <bradleysearle@gmail.com>
2022-11-12 06:21:45 +1000

    blog

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 
 
-FILENAME="codingpackets/templates/blog/ansible-from-the-start-to-the-beginning.jinja";
+FILENAME="codingpackets/templates/blog/aws-and-pulumi-with-go-getting-started.jinja";
 
 sed -i -e 's/cb\.codeBlock/text::code_block/g' $FILENAME
 
@@ -13,6 +13,8 @@ sed -i -e 's/{{ links\.blogPath\.uri }}/\/blog/g' $FILENAME
 sed -i -r -e 's/<a href="\/blog\/(.*?)">(.*?)<\/a>/{{ link::i(path="\/blog\/\1", description="\2") }}/g' $FILENAME
 
 # Exteranal Links
+sed -i -e 's/link.href(/link::e(/g' $FILENAME
+
 sed -i -r -e 's/<a href="(.*?)">(.*?)<\/a>/{{ link::e(path="\1", description="\2") }}/g' $FILENAME
 
 sed -i -r -e 's/<code>(.*?)<\/code>/{{ text::code(text="\1") }}/g' $FILENAME
