97ab7ae23369311f93d2fd0f39319418b925eb5b
Brad Searle <bradleysearle@gmail.com>
2022-11-14 07:06:21 +1000

    aws blogs

--- convert.sh
+++ convert.sh
@@ -1,4 +1,6 @@
-FILENAME="codingpackets/templates/blog/aws-s3-cloudfront-static-website-hosting.jinja";
+FILENAME="codingpackets/templates/blog/cloud-notes-aws-s3.jinja";
+
+sed -i -e 's/<\/span>/<\/span>\r\n/g' $FILENAME
 
 sed -i -e 's/cb\.codeBlock/text::code_block/g' $FILENAME
 
@@ -6,14 +8,21 @@ sed -i -e 's/nb\.noteBlock(/text::note_block(text=/g' $FILENAME
 
 sed -i -e 's/nb\.importantBlock(/text::important_block(text=/g' $FILENAME
 
+sed -i -e 's/nb\.warningBlock(/text::warning_block(text=/g' $FILENAME
+
 sed -i -e 's/bi\.boxedImage(/image::boxed(path=/g' $FILENAME
 
+sed -i -r -e 's/<span class="hljs-comment">(.*?)<\/span>/\1/g' $FILENAME
+
 # Internal Links
-sed -i -e 's/{{ links\.blogPath\.uri }}/\/blog/g' $FILENAME
+sed -i -e 's/{{ links.rootPath.uri }}/\//g' $FILENAME
+sed -i -e 's/{{ links.blogPath.uri }}/\/blog/g' $FILENAME
 sed -i -r -e 's/<a href="\/blog\/(.*?)">(.*?)<\/a>/{{ link::i(path="\/blog\/\1", description="\2") }}/g' $FILENAME
 
 # Exteranal Links
 sed -i -e 's/link.href(/link::e(/g' $FILENAME
+
+sed -i -r -e 's/<a href="(.*?)">(.*?)<\/a>/{{ link::e(path="\1", description="\2") }}/g' $FILENAME
 
 sed -i -r -e 's/<a href="(.*?)">(.*?)<\/a>/{{ link::e(path="\1", description="\2") }}/g' $FILENAME
 
@@ -23,3 +32,6 @@ sed -i -r -e 's/<span class="emphasize-text-darkmode">(.*?)<\/span>/{{ text::emp
 
 sed -i -r -e 's/\{\% verbatim \%\}(.*?)\{\% endverbatim \%\}/\1/g' $FILENAME
 
+# Convert external to internal links
+sed -i -e 's/{{ link::e(path="\/"/{{ link::i(path="\/"/g' $FILENAME
+sed -i -e 's/{{ link::e(path="\/blog"/{{ link::i(path="\/blog"/g' $FILENAME
