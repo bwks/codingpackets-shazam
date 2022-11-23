d4264c9dceb5593a4287f20acff94f531ff94a8e
Brad Searle <bradleysearle@gmail.com>
2022-11-23 12:43:14 +1000

    blogs

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 files="\
-codingpackets/templates/blog/books-i-read.jinja \
+codingpackets/templates/blog/rails-generators-cheat-sheet.jinja \
 "
 
 for FILENAME in $files; do
@@ -11,6 +11,8 @@ for FILENAME in $files; do
   sed -i -e 's/<\/code>/<\/code>\r\n/g' $FILENAME
 
   sed -i -e 's/<\/kbd>/<\/kbd>\r\n/g' $FILENAME
+
+  sed -i -e 's/<\/a>/<\/a>\r\n/g' $FILENAME
 
   sed -i -e 's/cb\.codeBlock/text::code_block/g' $FILENAME
 
@@ -33,6 +35,7 @@ for FILENAME in $files; do
   sed -i -e 's/class="ipaddress"/class="font-mono"/g' $FILENAME
 
   sed -i -r -e 's/<span class="hljs-comment">(.*?)<\/span>/\1/g' $FILENAME
+  sed -i -r -e 's/<span class=\\"hljs-comment\\">(.*?)<\/span>/\1/g' $FILENAME
 
   sed -i -r -e 's/<div class="code-block-caption-darkmode text-center">[$[[:blank:]]]+<span class="code-block-title-darkmode">(.*?)<\/span>[$[[:blank:]]]+<\/div>[$[[:blank:]]]+<div class="code-block-left-darkmode">[$[[:blank:]]]+<pre><code class="text hljs">[$[[:blank:]]]+([.[[:blank:]]]+)<\/code>[$[[:blank:]]]+<\/pre>[$[[:blank:]]]+<\/div>/{{ text::code_block(\r\nheader="\1",\r\nlanguage="text",\r\ncode="\2"\r\n) }}/g' $FILENAME
 
@@ -67,6 +70,6 @@ for FILENAME in $files; do
   # Convert external to internal links
   sed -i -e 's/{{ link::e(path="\/"/{{ link::i(path="\/"/g' $FILENAME
   sed -i -e 's/{{ link::e(path="\/blog"/{{ link::i(path="\/blog"/g' $FILENAME
-
+  sed -i -e 's/target="_blank", description/, description/g' $FILENAME
 done
 
